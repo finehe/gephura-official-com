571fd00f91900311f35bebecaf695757f30313a5
finehe <finehe@foxmail.com>
2025-03-04 12:07:50 +0800

    Add contact section styling and contact information

---
+++ src/components/ContactSection.vue
@@ -0,0 +1,3 @@
+<a href="mailto:contact@gephura.com" style="color: #000000; font-weight: bold;">contact@gephura.com</a>
+
+<div style="color: #000000; font-weight: bold;">上海市浦东新区栗科路118号凯瑞大厦3号楼1层</div> 
